b8051f84602d9e2784ff0d1e85c76f2c74407fe1
Carlo Arellano <carlo.arellano@hotmail.com>
2025-08-24 00:39:52 +0800

    Works on the db context

---
+++ Northwind.DataContext/NorthwindContextExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Northwind.DataContext;
+
+public static class NorthwindContextExtensions
+{
+    /// <summary>
+    /// Adds NorthwindContext to the specified IServiceCollection. Uses the SqlServer database provider.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <param name="connectionString">Set to override the default.</param>
+    /// <returns>An IServiceCollection that can be used to add more services.</returns>
+    public static IServiceCollection AddNorthwindContext(this IServiceCollection services, string? connectionString = null)
+    {
+        if (connectionString is null)
+        {
+            SqlConnectionStringBuilder builder = new();
+            builder.DataSource = "CARLO-PC\\MSSQL_DEV_2022"; // SQL Edge in Docker.
+            builder.InitialCatalog = "NorthwindMVC";
+            builder.TrustServerCertificate = true;
+            builder.MultipleActiveResultSets = true;
+            // Because we want to fail faster. Default is 15 seconds.
+            builder.ConnectTimeout = 3;
+            // SQL Server authentication.
+            builder.UserID = Environment.GetEnvironmentVariable("NORTHWINDMVC_USER");
+            builder.Password = Environment.GetEnvironmentVariable("NORTHWINDMVC_PWD");
+            connectionString = builder.ConnectionString;
+        }
+
+        services.AddDbContext<NorthwindMvcContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+            options.LogTo(NorthwindContextLogger.WriteLine,
+            new[] { Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.CommandExecuting });
+        },
+        // Register with a transient lifetime to avoid concurrency
+        // issues with Blazor Server projects.
+            contextLifetime: ServiceLifetime.Transient,
+            optionsLifetime: ServiceLifetime.Transient);
+
+        return services;
+
+    }
+}
---
+++ Northwind.UnitTests/EntityModelTests.cs
@@ -0,0 +1,38 @@
+﻿using Northwind.DataContext;
+using Northwind.EntityModels;
+
+namespace Northwind.UnitTests;
+
+public class EntityModelTests
+{
+    [Fact]
+    public void DatabaseConnectTest()
+    {
+        using NorthwindMvcContext db = new();
+        Assert.True(db.Database.CanConnect());
+    }
+
+    [Fact]
+    public void CategoryCountTest()
+    {
+        using NorthwindMvcContext db = new();
+
+        int expected = 8;
+        int actual = db.Categories.Count();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void ProductId1IsChaiTest()
+    {
+        using NorthwindMvcContext db = new();
+
+        string expected = "Chai";
+
+        Product? product = db.Products.Find(keyValues: 1);
+        string actual = product?.ProductName ?? string.Empty;
+
+        Assert.Equal(expected, actual);
+    }
+}
